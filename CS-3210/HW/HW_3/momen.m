3132f5d1c724f6111ca49f574a562d66e99ea15f
Wasfi Momen <wasfimomenwim@gmail.com>
2016-06-29 15:23:50 -0400

    Added .imdone files and new HW for cs 3210

---
+++ CS-3210/HW/HW_3/momen.m
@@ -0,0 +1,11 @@
+/* TODO:10 Write an assembly language program to find the maximum of y= 3x^4 - 10x^2 - 15x.
+    for the range -4 <= x <= 6.
+
+    During each iteration, print out a message stating the current value of x and the corresponding value of y.
+
+    After all iterations, print out a final message stating the max value of y and the corresponding value of x (6) that generated  ymax. If there are multiple values of x that generate ymax, you only need to indicate one of them.
+
+    TODO:20 Also, make sure to define the values of the coefficents 3, 10, and 15 as constants a1, a2, and a3.
+
+    TODO:
+ */
