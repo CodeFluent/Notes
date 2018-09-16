1deab01385f0355326dc8b0124ce8695df54f429
CodeFluent <wasfimomenwim@gmail.com>
2018-09-15 23:29:19 -0400

    im an idiot sometimes, updated code for netwroks hw

--- CS-6220/HW2/tcpserver.py
+++ CS-6220/HW2/tcpserver.py
@@ -36,11 +36,16 @@ class TCPServer:
             self.socket.close()
             raise Exception('Error in setting up connection to host port.')
 
+    def accept(self):
+        """Start accepting incoming connections and data"""
+        return self.socket.accept()
+
     def server_bind(self):
         """Binds the socket to any available port"""
 
         """See Unix man socket(7) SO_REUSEPORT"""
         # self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
         try:
             # a tuple of the address and port is used for the bind function
             self.socket.bind((self.server_address, self.server_port))
@@ -52,10 +57,11 @@ class TCPServer:
     def server_start(self):
         """Start listening for any clients"""
         self.socket.listen(1)
+        self.print_details()  # print the details of the connection
 
     def print_details(self):
         """Print out the server details"""
-        print('SERVER READY @ %s' % self.socket.getsockname)
+        print('SERVER READY @ ', self.socket.getsockname())
 
     def __enter__(self):
         return self
@@ -70,4 +76,4 @@ class TCPServer:
 host = '127.0.0.1'
 port = 0  # the OS should choose an open port for us
 with TCPServer(host, port) as sock:
-    sock = TCPServer(host, port)
+    conn, addr = sock.accept()
