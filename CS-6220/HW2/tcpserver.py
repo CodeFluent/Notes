e9408faeefcd3c649ab0d507242424a5bd79df39
CodeFluent <wasfimomenwim@gmail.com>
2018-09-22 23:32:22 -0400

    multithread complete

--- CS-6220/HW2/tcpserver.py
+++ CS-6220/HW2/tcpserver.py
@@ -14,14 +14,12 @@ REQUIRMENTS
     - ~~Max 10 strings~~
     - ~~Client ask for termination~~ **"Appropiate message" to terminate is Ctrl+C**
     - ~~Limit reached, send message to client~~
-    - Multithreaded
+    - ~~Multithreaded~~
 
 """
 
 """
 ISSUES
-    - Don't bind port to specific number
-    - KeyboardInterrupt won't work until enter is pressed.
 """
 
 import socket
@@ -34,8 +32,8 @@ class TCPServer:
     """
     TCPServer class contains the necessary functions
     for the application by keeping track of the strings
-    and only binding the socket to the specified ports in 
-    the constructor. 
+    and only binding the socket to the specified ports in
+    the constructor.
 
     All other socket operations should
     be used by the .socket member access.
@@ -64,8 +62,8 @@ class TCPServer:
     def add_client_details(self, addr):
         """
         Add the tuples of our connected clients to a list.
-        This is used namely to connect back to clients for an auth check.
-        See auth_challenge() for more info.
+        This is not currently used, but would be great to 
+        gracefully except during shutdowns of client sockets.
         """
         if addr in self.client_details:
             return
@@ -78,17 +76,74 @@ class TCPServer:
         return client_string.upper()
 
     def bind(self):
-        """Binds the socket to any available port"""
-
-        """See Unix man socket(7) SO_REUSEPORT"""
-        # self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        """Binds the socket to a port"""
         try:
+            # option to reuse addresses for sockets
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             # a tuple of the address and port is used for the bind function
             self.socket.bind((self.server_address, self.server_port))
         except:
             self.socket.close()
             raise Exception(
                 'Error in binding socket to address and port specified.')
+
+    def process_clients(self, connection, address):
+        """
+        Main logic of the program. Takes care of the shutdow of
+        client sockets and termination of server when strings limit
+        is reached.
+        """
+        while True:
+            sentence = connection.recv(1024).decode()
+
+            # break out of while loop if shutdown request is given
+            if (sentence == "CLIENT REQUESTS SHUTDOWN"):
+                print("\nCLIENT HAS REQUESTED SHUTDOWN")
+                # 2 means SHUT_RDWR or disable read and write, Windows only takes numbers
+                connection.shutdown(2)
+                connection.close()
+                break
+
+            # break out of while loop if number of strings exceeds 10
+            if (self.get_num_of_strings() >= 10):
+                print(
+                    "STRINGS LIMIT REACHED, SHUTTING DOWN AND CLOSING SOCKET")
+                connection.sendall(
+                    "STRINGS LIMIT REACHED, CLOSING CONNECTION".encode())
+                connection.shutdown(2)
+                connection.close()
+                break
+
+            print("\nSentence is: ", sentence)
+            connection.sendall(
+                self.capitalize_string(sentence).encode() + "\nAwaiting next string...".encode())
+            print("Number of strings ",
+                  self.get_num_of_strings())
+        self.socket.close()
+
+    def start_client_listen(self):
+        """
+        Starts the mulithreaded listen for the 
+        clients. Also adds clients to a list of 
+        connected clients.
+
+        Every client added will go through 
+        here to be processed by process_clients()
+        """
+        try:
+            self.socket.listen(4)  # listen to 4 clients max
+            self.print_details()
+            while True:
+                conn, addr = self.socket.accept()
+                self.add_client_details(addr)
+                threading.Thread(target=self.process_clients,
+                                 args=(conn, addr)).start()
+
+            self.socket.close()
+        except KeyboardInterrupt:
+            print("\nExited by Ctrl+C.")
+            self.socket.close()
+            sys.exit(1)
 
     def print_details(self):
         """Print out the server details"""
@@ -107,49 +162,4 @@ host = "127.0.0.1"
 # port = 0  # the OS should choose an open port for us
 port = 65434
 
-
-def thread_server(socket, connection):
-    while True:
-        sentence = connection.recv(1024).decode()
-        """Turn if loops into a function"""
-        # break out of while loop if shutdown request is given
-        if (sentence == "CLIENT REQUESTS SHUTDOWN"):
-            print("\nCLIENT HAS REQUESTED SHUTDOWN")
-            # 2 means SHUT_RDWR or disable read and write, Windows only takes numbers
-            connection.shutdown(2)
-            connection.close()
-            break
-        # break out of while loop if number of strings exceeds 10
-        if (socket.get_num_of_strings() >= 10):
-            print(
-                "STRINGS LIMIT REACHED, SHUTTING DOWN AND CLOSING SOCKET")
-            connection.sendall(
-                "STRINGS LIMIT REACHED, CLOSING CONNECTION".encode())
-            connection.shutdown(2)
-            connection.close()
-            break
-        print("\nSentence is: ", sentence)
-        connection.sendall(
-            socket.capitalize_string(sentence).encode() + "\nAwaiting next string...".encode())
-        print("Number of strings ",
-              socket.get_num_of_strings())
-
-
-def main():
-    try:
-        server_sock = TCPServer(host, port)
-        server_sock.socket.listen()
-        server_sock.print_details()
-        while True:
-            conn, addr = server_sock.socket.accept()
-            server_sock.add_client_details(addr)
-            thread_server(server_sock, conn)
-            break
-        server_sock.socket.close()
-    except KeyboardInterrupt:
-        print("\nExited by Ctrl+C.")
-        server_sock.socket.close()
-        sys.exit(1)
-
-
-main()
+TCPServer(host, port).start_client_listen()
