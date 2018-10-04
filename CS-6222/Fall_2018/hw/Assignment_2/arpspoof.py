ea5bdaf24d196ef5f12ab93f2938372b61bc1b67
CodeFluent <wasfimomenwim@gmail.com>
2018-10-04 17:22:34 -0400

    functions added

--- CS-6222/Fall_2018/hw/Assignment_2/arpspoof.py
+++ CS-6222/Fall_2018/hw/Assignment_2/arpspoof.py
@@ -9,6 +9,22 @@ def record_client(packet):
         clients[packet.psrc] = packet.hwsrc
 
 
+def identify_attacker(packet):
+    # exclude the  standard way for ethernet packets to broadcast to all clients
+    if packet.dst != 'ff:ff:ff:ff:ff:ff':
+        # The attacker's Eth layer p.src will not match the ARP layer p.hwsrc
+        if (packet.src != packet.hwsrc):
+            return packet.src
+    else:
+        return None
+
+
+def output_warning(packet, packet_number):
+    if (packet != None):
+        print("Offending MAC address: ", packet,
+              "Packet number:", packet_number)
+
+
 def main():
     # load file from same directory
     a = rdpcap('arpspoofing.pcap')
@@ -16,17 +32,17 @@ def main():
     # a.show()  # to show all packets using nsummary
 
     """
-    What follows are the scapy packet attributes and their Wireshark equivalents in parentheses.
-        - p.src is also p.hwsrc as SourceIPField or ARPSourceMacField, respectively. (SenderIPAddress)
-        - p.dst is also p.hwdst as DestMacField or MACField, respectively. (TargetMACAddress)
+    What follows are the scapy packet attributes (by layer) and their Wireshark equivalents in parentheses.
+        - p.src (Eth) is also p.psrc, p.hwsrc (ARP) as SourceMACField, SourceIPField, or ARPSourceMacField, respectively. (SenderIPAddress)
+        - p.dst (Eth) is also p.pdst, p.hwdst (ARP) as DestMacField, IPField, or MACField, respectively. (TargetMACAddress)
         - p.pdst is just an IPField, normally the destination (TargetIPAddress)
     """
 
-    # for more info on packets
+    # # for more info on packets
     # for idx, p in enumerate(a):
     #     p.show()
 
-    # for more info on fields
+    # # for more info on fields
     # for idx, p in enumerate(a):
     #     ls(p)
 
@@ -35,8 +51,10 @@ def main():
             # get a list of clients and their MAC addresses
             record_client(p)
             # ARP attacker will be telling every other client that they are a certain IP address to spoof
+            attacker = identify_attacker(p)
             # Output a warning with the attacker's MAC address and the offending packet number
-            p.getlayer(ARP).show()
+            output_warning(attacker, idx+1)
+            # p.getlayer(ARP).show()
             # print(idx + 1)  # packet number
 
     print(clients)
