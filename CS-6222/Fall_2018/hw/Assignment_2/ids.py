dcbabb20e25b6561e4693be2dbdb5a44b931ded2
CodeFluent <wasfimomenwim@gmail.com>
2018-10-05 23:33:56 -0400

    Added comments and completed

--- CS-6222/Fall_2018/hw/Assignment_2/ids.py
+++ CS-6222/Fall_2018/hw/Assignment_2/ids.py
@@ -3,8 +3,27 @@
 ids.py
 
 A program to take a pcap file and use the scapy library to detect a TCP SYN port scan.
+To run this program, you will need python 3.0+ and the scapy library installed.
+You can install scapy with `pip install scapy`.
 
 Made by Wasfi Momen.
+
+"""
+
+"""
+REQUIREMENTS:
+    - ~~Detect a port scan when an attacker sends more than 100 TCP SYN packets to a target system~~
+    - ~~Detect a port scan when an attacker sends more than 100 UDP packets to a target system~~
+    - ~~Output a warning including the offending source IP address, the victim destination IP address
+    , and the offending packet numbers.~~
+    ALL COMPLETE
+
+ISSUES:
+    - Should've probably asked, but the instructions said "TCP SYN or UDP" and I took that as 
+    inclusive. So both TCP SYN and UDP packets will increment the ports_hit.
+    - This program will fail to detect anything other than the clients listed in the clients
+    dictionary.
+    - Ports_hit will also increment on valid connections that exchange application data.
 
 """
 
@@ -67,17 +86,12 @@ def main():
     # a.show()  # to show all packets using nsummary format
 
     for idx, p in enumerate(a):
-        if (p.haslayer(TCP) and p['TCP'].flags == 'S'):
+        if ((p.haslayer(TCP) and p['TCP'].flags == 'S') or (p.haslayer(UDP))):
             # see if destination port is unique in the pcap
             if (unique_port(p)):
                 # find the packet's source ip in the dictionary and increment the number of ports hit
                 increment_ports_hit(p['IP'].src)
                 identify_attacker(p, idx + 1)
-
-            # Output a warning
-            # output_warning(src_ip, dest_ip, idx+1)
-        elif (p.haslayer(UDP)):
-            pass
 
     # print(clients)  # print out the list of clients
 
