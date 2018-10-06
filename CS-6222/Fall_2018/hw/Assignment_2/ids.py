f9aa77c13162715a76a224f6475427f2718eafa1
CodeFluent <wasfimomenwim@gmail.com>
2018-10-05 22:17:22 -0400

    comments and code

--- CS-6222/Fall_2018/hw/Assignment_2/ids.py
+++ CS-6222/Fall_2018/hw/Assignment_2/ids.py
@@ -11,18 +11,21 @@ Made by Wasfi Momen.
 from scapy.utils import rdpcap
 from scapy.layers.all import *
 
+# load file from same directory
+a = rdpcap('portscan.pcap')
+
 clients = {
-    "192.168.0.100": "7c:d1:c3:94:9e:b8",
+    "192.168.0.100": ["7c:d1:c3:94:9e:b8", 0],
     # typo in assignment .103 should be .101 and MAC is 60:fe not d8:96
-    "192.168.0.101": "60:fe:c5:9e:63:3c",
-    "192.168.0.1": "f8:1a:67:cd:57:6e"
+    "192.168.0.101": ["60:fe:c5:9e:63:3c", 0],
+    "192.168.0.1": ["f8:1a:67:cd:57:6e", 0]
 }
 
 
 def get_key(value):
     """
-    Returns the first key given the value. Cuz dictionaries aren't "intended" to do that. 
-    Used to return the ip from a given mac address registered in the clients dictionary. 
+    Returns the first key given the value. Cuz dictionaries aren't "intended" to do that.
+    Used to return the ip from a given mac address registered in the clients dictionary.
     """
     for ip, mac in clients.items():
         if (mac == value):
@@ -35,14 +38,23 @@ def get_key(value):
 #         clients[packet.psrc] = packet.hwsrc
 
 
-def identify_attacker(p):
-    """Returns the source mac address of the attacker. Will return None if packet is legit"""
-    # exclude the  standard way for ethernet packets to broadcast to all clients
-    if p.dst != 'ff:ff:ff:ff:ff:ff':
-        # if the ip and mac of the packet is not the same as the the registered ones
-        # then we return the packet's mac address. This mac address is our advesary.
-        if not((get_key(p.pdst) == p.pdst and clients.get(p.dst) == p.dst)):
-            return p.src
+def record_ports_hit(packet, packet_number):
+    # given packet and packet number...
+    # get the destination port number for the TCP SYN packet
+    dest_port = packet.dport
+    # see if destination port is unique in the pcap
+    unique_port(dest_port)
+    # find the packet's source ip in the dictionary
+    # increment the number of ports hit
+    # future feature: before incrementing, follow convo of packet to see if it is actually a legit connection. If legit, don't increment.
+    pass
+
+
+def unique_port(port_num):
+    # (check all past packets for port vs check all mentions of port in pcap)
+    # check all past packets to see if port_num was previously used. Return false if already used.
+
+    pass
 
 
 def output_warning(source_ip_address, destination_ip_address, packet_number):
@@ -84,19 +96,20 @@ def output_warning(source_ip_address, destination_ip_address, packet_number):
 
 
 def main():
-    # load file from same directory
-    a = rdpcap('portscan.pcap')
 
     # a.show()  # to show all packets using nsummary format
 
     # experiments()  # for experimental/learning purposes
 
     for idx, p in enumerate(a):
-        if (p.haslayer(ARP)):
-            # get the source mac address of the attacker
-            attacker = identify_attacker(p)
+        if (p.haslayer(TCP) and p['TCP'].flags == 'S'):
+            # record_ports_hit(packet, idx + 1)
+            p.show()
+            # attacker = identify_attacker(p)
             # Output a warning
-            output_warning(src_ip, dest_ip, idx+1)
+            # output_warning(src_ip, dest_ip, idx+1)
+        elif (p.haslayer(UDP)):
+            pass
 
     # print(clients)  # print out the list of clients
 
