118c5790530efb82dc128e877e9b7a14395d7f12
CodeFluent <wasfimomenwim@gmail.com>
2018-10-18 18:46:31 -0400

    got it working

---
+++ CS-6220/hw/HW3/httpserver.py
@@ -0,0 +1,65 @@
+"""
+
+httpserver.py
+
+A TCP-based HTTP server made in python.
+Some code from Python's http.server module.
+Made by Wasfi Momen.
+
+"""
+
+"""
+REQUIREMENTS
+    - Starts a HTTP server on any port other than port 80
+    - Shows 404 if a page is not found
+    - Shows 504 if server is down
+
+"""
+
+import socket
+import threading
+import socketserver
+import sys
+
+from http.server import BaseHTTPRequestHandler
+
+
+DEFAULT_PAGE = """\
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+        "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+        <title>Hello</title>
+    </head>
+    <body>
+        <h1>Welcome to the HTTP Server!</h1>
+        <p>Try to go to an unknown page like <a href="wtf.html">here.</a></p>
+        <p>Or try to intiate a timeout here.</p>
+    </body>
+</html>
+"""
+
+
+
+class Page_Handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if (self.path == "/"):
+            self.wfile.write(DEFAULT_PAGE.encode("utf-8"))
+        return 
+
+ 
+
+def main():
+    host = "127.0.0.1"
+    port = 8343
+    
+    
+    server = socketserver.TCPServer((host, port), Page_Handler)
+    server.serve_forever()
+    
+
+
+if __name__ == '__main__':
+    main()
