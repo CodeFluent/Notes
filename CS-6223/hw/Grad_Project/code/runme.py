a5d8b7205eea062103a080c72f859d970f737b83
CodeFluent <wasfimomenwim@gmail.com>
2018-11-30 19:34:59 -0500

    working

--- CS-6223/hw/Grad_Project/code/runme.py
+++ CS-6223/hw/Grad_Project/code/runme.py
@@ -2,7 +2,7 @@
 
 runme.py
 
-A project to use the UCI machine learning dataset and add privacy to the values.
+Runner file to use the mechanism to protect values in a dataset.
 
 Made by Wasfi Momen.
 
@@ -18,24 +18,43 @@ REQUIREMENTS:
 
 import numpy as np
 import pandas as pd
+from numpy.random import random
+
+random.seed(6223) # so we can make random values consistent
+
+"""
+The Randomized Response Mechanism. Will perturb data values with probability p and q.
+
+Same setup as Assignment 2, we flip a coin and if it's tails (prob_p) we give the data value d. 
+However, if it's heads (prob_q), then we flip another coin and give answer "1" for heads and 
+"0" for tails. This means we have values 0 <= d <= 1.
+
+"""
+def RR(p, q, data):
+    if (random() < p):
+        return data
+    elif (random() < q):
+        return 1
+    else:
+        return 0
 
 
-dataframe = pd.read_csv("data/adult.data", header=None)
 
-dataframe.columns = [
-    "Age", "Workclass", "Fnlwgt", "Education", "Education-Number",
-    "Marital-status", "Occupation", "Relationship", "Race", "Gender",
-    "Capital-Gain", "Capital-Loss", "Hoursperweek", "Native-Country", "Class"
-]
+def clean_up():
+    # read csv file
+    dataframe = pd.read_csv("data/adult.data")
 
-# input_csv = pd.read_csv("data/adult.data", header=None)
+    # assign columns for the DataFrame
+    dataframe.columns = [
+        "Age", "Workclass", "Fnlwgt", "Education", "Education-Number",
+        "Marital-status", "Occupation", "Relationship", "Race", "Gender",
+        "Capital-Gain", "Capital-Loss", "Hoursperweek", "Native-Country", "Class"
+    ]
 
-# dataframe = pd.DataFrame(input_csv, columns= [
-#     "Age", "Workclass", "Fnlwgt", "Education", "Education-Number",
-#     "Marital-status", "Occupation", "Relationship", "Race", "Gender",
-#     "Capital-Gain", "Capital-Loss", "Hoursperweek", "Native-Country", "Class"
-# ])
+    return dataframe
 
-print(dataframe)
+    # print(dataframe)
 
 
+df = clean_up()
+print(df)
