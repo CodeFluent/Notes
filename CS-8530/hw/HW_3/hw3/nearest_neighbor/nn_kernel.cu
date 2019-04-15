365c53c0d50358ccb0ec3561199ef99266dbcd29
CodeFluent <wasfimomenwim@gmail.com>
2019-04-15 03:56:06 -0400

    gitignore working, I hate using this IDE

---
+++ CS-8530/hw/HW_3/hw3/nearest_neighbor/nn_kernel.cu
@@ -0,0 +1,12 @@
+// includes, system
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+// includes CUDA
+#include <cuda_runtime.h>
+
+//// includes, project
+//#include <helper_cuda.h>
+//#include <helper_functions.h> // helper functions for SDK examples
